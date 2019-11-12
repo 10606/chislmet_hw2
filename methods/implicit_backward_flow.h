e968d9ef6519bc4867290b989cc822d8f9fed097
10606 <gav_sbor@mail.ru>
2019-11-12 21:27:50 +0300

    init methods

---
+++ methods/implicit_backward_flow.h
@@ -0,0 +1,32 @@
+#ifndef IMPLICIT_BACKWARD_FLOW_H
+#define IMPLICIT_BACKWARD_FLOW_H
+
+#include <vector>
+
+std::vector <std::vector <double> >  // T[x][t]
+implicit_backward_flow
+(
+    double delta_x,
+    double delta_t,
+    double u,
+    double cappa,
+    std::pair <double, double> x_range, 
+    std::pair <double, double> t_range, 
+    std::vector <double> T_t0_values
+)
+{
+    size_t x_size = (delta_x + x_range.second - x_range.first) / delta_x;
+    size_t t_size = (delta_t + t_range.second - t_range.first) / delta_t;
+
+    std::vector <std::vector <double> > answer(x_size, std::vector <double> (t_size));
+    for (size_t i = 0; i != x_size; ++i)
+    {
+        answer[i][0] = T_t0_values[i];
+    }
+
+    //TODO
+
+    return answer;
+}
+
+#endif
---
+++ utils.h
@@ -0,0 +1,36 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <vector>
+
+/*
+
+d_T/d_t + u * d_T/d_x - e d_2_T/d_2_x  =  0
+
+x in [a, b]
+t in [0, tau]
+
+
+u 
+e
+a b 
+tau
+T(0, x)
+
+delta_t 
+delta_x
+
+*/
+
+typedef std::vector <std::vector <double> > (* method_type)  // T[x][t]
+(
+    double delta_x,
+    double delta_t,
+    double u,
+    double cappa,
+    std::pair <double, double> x_range, 
+    std::pair <double, double> t_range, 
+    std::vector <double> T_t0_values
+);
+
+#endif
